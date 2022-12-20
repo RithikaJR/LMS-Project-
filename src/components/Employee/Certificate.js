eaf1f166f9b5d353286a93fde39ae4192bd79625
drishya.t <drishya.t@experionglobal.com>
2022-12-20 18:30:24 +0530

    20-12-22 18:30

--- src/components/Employee/Certificate.js
+++ src/components/Employee/Certificate.js
@@ -1,46 +1,44 @@
 
-import React, { Component } from "react";
+import React, {useState} from "react";
 import { exportComponentAsPNG } from "react-component-export-image";
 import Certificatepng from '../images/Certificatepng.png';
 import classes from "./Certificate.module.css";
+ 
+const Certificate = () => {
 
-
-class Certificate extends Component{
-  certificateWrapper = React.createRef();
-  state = {
-    Name: "",
-    course:""
-  };
-  render() {
-    return (
-      <div className={classes.app}>
-        <div className={classes.meta}>
-          <h1>Certificate</h1>
-      
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              exportComponentAsPNG(this.certificateWrapper);
-              
-            }}
-          >
+  const [certificateWrapper, setdata] = useState({
+       Name: "vf",
+      course:""})
+ 
+  
+  return (
+    <div className={classes.app}>
+      <div className={classes.meta}>
+        <h1>Certificate</h1>
+    
+        <button
+          onClick={(e) => {
+            e.preventDefault();
             
-            Download
-          </button>
-        </div>
+            exportComponentAsPNG(certificateWrapper);
+          }}
+        > 
+          Download
+        </button>
+      </div>
 
-        <div className={classes.downloadWrapper } ref={this.certificateWrapper}>
-          <div className={classes.certificateWrapper}>
-            <p1>{this.state.Name}</p1>
-            <p2> {this.state.course}</p2>
-           
-            <img src={Certificatepng}/>
-          </div>
+      <div className={classes.downloadWrapper } ref={certificateWrapper}>
+        <div className={classes.certificateWrapper}>
+          <p1>{certificateWrapper.Name}</p1>
+          <p2>{certificateWrapper.course}</p2>
+         
+          <img src={Certificatepng}/>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+  
+        }
 
 export default Certificate;
 
